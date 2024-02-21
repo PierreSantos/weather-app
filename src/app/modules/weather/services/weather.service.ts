384e4e20a5489c8abb7b2e78765787a15597f291
Pierre Santos <pierre_603368@trt5.jus.br>
2024-02-21 09:16:28 -0300

    projeto de previsão do tempo feito com Angular

---
+++ src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private apiKey = '4b798fc1196fa028407337186e773af7';
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
+    {})
+  }
+}
